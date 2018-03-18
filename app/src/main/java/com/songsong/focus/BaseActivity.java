61bc25fe311a25f5ba776d76e6d0c0d6e96e22ef
songsong <374155400@qq.com>
2018-03-18 16:16:26 +0800

    test2

---
+++ app/src/main/java/com/songsong/focus/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.songsong.focus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
+
+/**
+ * Created by a on 2018/3/18.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    public Toolbar mToolbar;
+
+    public void setTitle(String title, int type) {
+
+
+    }
+
+}
